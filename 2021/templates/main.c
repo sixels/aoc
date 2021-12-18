57b014b2e6ac00702b48447f04d37feccc03fc7e
Sixels <sixels@protonmail.com>
2021-12-17 21:22:56 -0300

    Add read_file to main.c template

--- 2021/templates/main.c
+++ 2021/templates/main.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<assert.h>
-#include<string.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #if 1
 #define INPUT_FILE "./src/#DAY#/sample.txt"
@@ -9,19 +9,35 @@
 #define INPUT_FILE "./src/#DAY#/inputs.txt"
 #endif
 
-void part_one() {
+char *read_file() {
+  FILE *file = fopen(INPUT_FILE, "r");
+
+  if (file == NULL) {
+    fprintf(stderr, "Could not find file %s\n", INPUT_FILE);
+    exit(1);
+  }
+
+  fseek(file, 0, SEEK_END);
+  size_t file_size = ftell(file);
+  rewind(file);
+
+  char *content = malloc(file_size + 1);
+  fread(content, sizeof(char), file_size, file);
+
+  return content;
 }
 
-void part_two() {
-}
+void part_one() {}
+
+void part_two() {}
 
 int main(void) {
 #ifdef PARTONE
-    printf("Running part one...\n");
-    part_one();
+  printf("Running part one...\n");
+  part_one();
 #else
-    printf("Running part two...\n");
-    part_two();
+  printf("Running part two...\n");
+  part_two();
 #endif
-    return 0;
+  return 0;
 }
