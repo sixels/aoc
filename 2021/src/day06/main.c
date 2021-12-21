30c197bd1d9f3f03909c98b87701c788da03f5b3
Sixels <sixels@protonmail.com>
2021-12-21 14:50:18 -0300

    Day 6 complete

--- 2021/src/day06/main.c
+++ 2021/src/day06/main.c
@@ -15,6 +15,13 @@
 
 #define N 9
 
+void dump_days(uint64_t days[N]) {
+  for (int i = 0; i < N - 1; i++) {
+    printf("%lu,", days[i]);
+  }
+  printf("%lu\n===========\n", days[N - 1]);
+}
+
 void part_one(char *input) {
   const int n_days = 80;
 
@@ -43,7 +50,37 @@ void part_one(char *input) {
   printf("Total fishs after %d days: %d\n", n_days, total_fishs);
 }
 
-void part_two(char *input) {}
+void part_two(char *input) {
+  uint64_t total_fishs = 0;
+  const int n_days = 256;
+
+  uint64_t days[N] = {};
+  memset(days, 0, N * sizeof(uint64_t));
+
+  // get the initial state
+  do {
+    int fish = next_num(&input);
+    days[fish]++;
+    total_fishs++;
+  } while (*input != 0);
+
+  // dump_days(days);
+
+  // simulate
+  for (int i = 0; i < n_days; i++) {
+    const uint64_t last_day = days[0];
+    for (int d = 0; d < N - 1; d++) {
+      days[d] = days[d + 1];
+    }
+    days[6] += last_day;
+    days[8] = last_day;
+    total_fishs += last_day;
+    // dump_days(days);
+    // printf("day %d: %ld\n", i+1, total_fishs);
+  }
+
+  printf("Total fishs after %d days: %lu\n", n_days, total_fishs);
+}
 
 int main(void) {
   char *input = read_file(INPUT_FILE);
