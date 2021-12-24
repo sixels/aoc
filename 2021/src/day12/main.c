cba24eaf9ec328719d436c320b01d16809f0f750
Sixels <sixels@protonmail.com>
2021-12-24 20:35:28 -0300

    Day 12 complete

--- 2021/src/day12/main.c
+++ 2021/src/day12/main.c
@@ -139,7 +139,91 @@ void part_one(char *input) {
   printf("NUMBER OF PATHS: %d\n", n);
 }
 
-void part_two(char *input) {}
+int visit_nodes2(Graph *node, int did_small_twice) {
+  int n = 0;
+
+  visits++;
+
+  if (!node || node == nodes[0]) goto end_visit2;
+  if (node == nodes[1]) {
+    n = 1;
+    goto end_visit2;
+  }
+
+  if (node->visited && !node->is_big) {
+    if (did_small_twice) goto end_visit2;
+    for (size_t i = 0; i < node->siblings_len; i++) {
+      // Python's string multiplication would be awesome here LOL
+      // printf("%.*s %c-->%c\n", visits * 2,
+      //        "                                                          ",
+      //        node_name(node), node_name(node->siblings[i]));
+
+      n += visit_nodes2(node->siblings[i], true);
+    }
+  } else {
+    node->visited = true;
+
+    for (size_t i = 0; i < node->siblings_len; i++) {
+      // Python's string multiplication would be awesome here LOL
+      // printf("%.*s %c-->%c\n", visits * 2,
+      //        "                                                          ",
+      //        node_name(node), node_name(node->siblings[i]));
+
+      n += visit_nodes2(node->siblings[i], did_small_twice);
+    }
+    node->visited = false;
+  }
+
+end_visit2:
+  visits--;
+  return n;
+}
+
+void part_two(char *input) {
+  Graph *start = create_node(false);
+  Graph *end = create_node(false);
+
+  memset(nodes, 0, sizeof(nodes));
+
+  nodes[0] = start;
+  nodes[1] = end;
+
+  // parse the inputs
+  while (*input) {
+    char *from = input;
+    while (*input != '-') {
+      input++;
+    }
+    size_t from_len = input - from;
+    input++;
+
+    Graph *from_node = node_from_name(from, from_len);
+
+    char *to = input;
+    while (*input && *input != '\n') {
+      input++;
+    }
+    size_t to_len = input - to;
+    if (*input == '\n') input++;
+
+    Graph *to_node = node_from_name(to, to_len);
+
+    from_node->siblings[from_node->siblings_len++] = to_node;
+    to_node->siblings[to_node->siblings_len++] = from_node;
+  }
+
+  int n = 0;
+  for (size_t i = 0; i < start->siblings_len; i++) {
+    printf("%c-->%c\n", node_name(start), node_name(start->siblings[i]));
+    n += visit_nodes2(start->siblings[i], false);
+  }
+
+  for (int i = 0; i < 128; i++) {
+    if (nodes[i]) free(nodes[i]);
+  }
+
+  printf("NUMBER OF PATHS: %d\n", n);
+}
 
 int main(void) {
   char *input = read_file(INPUT_FILE);
