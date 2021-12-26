c08dbe9bb279a0af3a557485fff0147e6537183c
Sixels <sixels@protonmail.com>
2021-12-26 14:41:39 -0300

    Fix `min()` and `max()`

--- 2021/lib/aoclib.h
+++ 2021/lib/aoclib.h
@@ -1,8 +1,8 @@
 #ifndef AOC_LIB_H
 #define AOC_LIB_H
 
-#define max(a, b) a < b ? b : a
-#define min(a, b) a < b ? a : b
+#define max(a, b) (a < b ? b : a)
+#define min(a, b) (a < b ? a : b)
 
 char *read_file(char *);
 int next_num(char **);
